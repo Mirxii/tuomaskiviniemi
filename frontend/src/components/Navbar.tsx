0eece31975f900f7eb2f5a53f4f7b6e8bcf7f40f
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-03-28 22:04:22 +0300

    Changed base font, hover styles to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,10 +4,16 @@ const Navbar = () => {
   return (
     <nav>
       <div className="flex p-4 justify-center shadow-md text-lg">
-        <Link className="px-2" to="/">
+        <Link
+          className="px-2 rounded-md hover:bg-green-500 hover:text-gray-800 hover:bg-opacity-40 hover:cursor-pointer"
+          to="/"
+        >
           Home
         </Link>
-        <Link className="px-2" to="/portfolio">
+        <Link
+          className="px-2 rounded-md hover:bg-green-500 hover:text-gray-800 hover:bg-opacity-40 hover:cursor-pointer"
+          to="/portfolio"
+        >
           Portfolio
         </Link>
       </div>
