c4f949acd4a2dcad9b56cd297daf2f97f8e65aed
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-03-31 17:36:55 +0300

    Added socials to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,46 @@
 import { Link } from 'react-router-dom';
 
+import github from '../assets/github.svg';
+import linkedin from '../assets/linkedin.svg';
+
 const Navbar = () => {
   return (
-    <nav>
-      <div className="flex justify-center p-4 text-lg shadow-md">
-        <Link
-          className="rounded-md px-2 hover:cursor-pointer hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
-          to="/"
-        >
-          Home
-        </Link>
-        <Link
-          className="rounded-md px-2 hover:cursor-pointer hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
-          to="/portfolio"
-        >
-          Portfolio
-        </Link>
+    <div className="flex p-6 text-lg shadow-md ">
+      <div className="flex w-3/4 items-center justify-center">
+        <nav>
+          <Link
+            className="rounded-md px-2 hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="rounded-md px-2 hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
+            to="/portfolio"
+          >
+            Portfolio
+          </Link>
+        </nav>
       </div>
-    </nav>
+      <div className="flex w-1/4 justify-center">
+        <nav className="flex">
+          <Link
+            className="px-2 duration-100 ease-in-out hover:-translate-y-0.5"
+            to="https://github.com/Mirxii"
+            target={'_blank'}
+          >
+            <img className="h-8 w-8" src={github} alt="GitHub" />
+          </Link>
+          <Link
+            className="px-2 duration-100 ease-in-out hover:-translate-y-0.5"
+            to="https://www.linkedin.com/in/tuomas-kiviniemi-755572172/"
+            target={'_blank'}
+          >
+            <img className="h-8 w-8" src={linkedin} alt="LinkedIn" />
+          </Link>
+        </nav>
+      </div>
+    </div>
   );
 };
 
