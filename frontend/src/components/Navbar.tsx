9816014ecae5eb817c44ddfc8795c552404814e5
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-03-31 14:31:09 +0300

    Added tailwind prettier plugin to autosort classes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex p-4 justify-center shadow-md text-lg">
+      <div className="flex justify-center p-4 text-lg shadow-md">
         <Link
-          className="px-2 rounded-md hover:bg-green-500 hover:text-gray-800 hover:bg-opacity-40 hover:cursor-pointer"
+          className="rounded-md px-2 hover:cursor-pointer hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
           to="/"
         >
           Home
         </Link>
         <Link
-          className="px-2 rounded-md hover:bg-green-500 hover:text-gray-800 hover:bg-opacity-40 hover:cursor-pointer"
+          className="rounded-md px-2 hover:cursor-pointer hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
           to="/portfolio"
         >
           Portfolio
