f9e6f6de23d39e6b3892dde620e553c8ec3ce8cc
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-04-01 16:19:55 +0300

    More styling on navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,40 +5,36 @@ import linkedin from '../assets/linkedin.svg';
 
 const Navbar = () => {
   return (
-    <div className="flex p-6 text-lg shadow-md ">
-      <div className="flex w-3/4 items-center justify-center">
-        <nav>
-          <Link
-            className="rounded-md px-2 hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
-            to="/"
-          >
-            Home
-          </Link>
-          <Link
-            className="rounded-md px-2 hover:bg-green-500 hover:bg-opacity-40 hover:text-gray-800"
-            to="/portfolio"
-          >
-            Portfolio
-          </Link>
-        </nav>
+    <div className="fixed flex w-full bg-white bg-opacity-50 p-4 text-lg shadow-md  backdrop-blur-sm">
+      <div className="flex w-3/4 items-center justify-center gap-2">
+        <Link
+          className="rounded-lg px-2 py-1 transition duration-200 hover:bg-slate-200 hover:bg-opacity-50 hover:text-sky-900 hover:shadow-md"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="rounded-lg px-2 py-1 transition duration-200 hover:bg-slate-200 hover:bg-opacity-50 hover:text-sky-900 hover:shadow-md"
+          to="/portfolio"
+        >
+          Portfolio
+        </Link>
       </div>
-      <div className="flex w-1/4 justify-center">
-        <nav className="flex">
-          <Link
-            className="px-2 duration-100 ease-in-out hover:-translate-y-0.5"
-            to="https://github.com/Mirxii"
-            target={'_blank'}
-          >
-            <img className="h-8 w-8" src={github} alt="GitHub" />
-          </Link>
-          <Link
-            className="px-2 duration-100 ease-in-out hover:-translate-y-0.5"
-            to="https://www.linkedin.com/in/tuomas-kiviniemi-755572172/"
-            target={'_blank'}
-          >
-            <img className="h-8 w-8" src={linkedin} alt="LinkedIn" />
-          </Link>
-        </nav>
+      <div className="flex w-1/4 items-center justify-center gap-3">
+        <Link
+          className="duration-100 ease-in-out hover:-translate-y-0.5"
+          to="https://github.com/Mirxii"
+          target={'_blank'}
+        >
+          <img className="h-8 w-8" src={github} alt="GitHub" />
+        </Link>
+        <Link
+          className="duration-100 ease-in-out hover:-translate-y-0.5"
+          to="https://www.linkedin.com/in/tuomas-kiviniemi-755572172/"
+          target={'_blank'}
+        >
+          <img className="h-8 w-8" src={linkedin} alt="LinkedIn" />
+        </Link>
       </div>
     </div>
   );
