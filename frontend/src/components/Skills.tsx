5d27b47dadcc74fc5546397b9dddb8518bbebcb5
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-04-07 18:45:41 +0300

    Changed skills section

--- frontend/src/components/Skills.tsx
+++ frontend/src/components/Skills.tsx
@@ -1,4 +1,4 @@
-const LANGUAGES = [
+const skillsList = [
   'HTML',
   'CSS',
   'JavaScript',
@@ -6,22 +6,23 @@ const LANGUAGES = [
   'Java',
   'C++',
   'Python',
-];
-
-const FRAMEWORKS = [
   'React',
   'React Native',
   'Node.js',
   'Express',
   'TailwindCSS',
   'Jest',
+  'Cypress',
+  'MySQL',
+  'PostgreSQL',
+  'Git',
+  'GitHub Actions',
+  'Docker',
+  'Vite',
+  'Linux',
+  'Prometheus',
+  'Grafana',
 ];
-
-const DATABASES = ['MySQL', 'PostgreSQL'];
-
-const TOOLS = ['Git', 'GitHub Actions', 'Docker', 'Jira', 'Vite', 'Cypress'];
-
-const OTHERS = ['Agile', 'Scrum', 'Linux', 'Prometheus', 'Grafana'];
 
 const Skills = () => {
   return (
@@ -31,67 +32,15 @@ const Skills = () => {
       </div>
       <div className="mx-auto mt-12 flex flex-col text-center md:my-auto md:w-3/5 md:text-left">
         <div className="">
-          <h1 className="text-xl">Programming Languages</h1>
+          <h1 className="text-xl">Technologies</h1>
           <hr className="mx-auto my-2 h-0.5 w-5/6 bg-gradient-to-r from-slate-50 via-slate-300 to-slate-50 md:mx-0 md:from-slate-300 md:via-slate-300" />
           <div className="flex flex-wrap justify-center gap-2 py-1 pr-1 md:justify-start">
-            {LANGUAGES.map((language) => (
+            {skillsList.map((skill) => (
               <div
-                key={language}
+                key={skill}
                 className="rounded bg-slate-200 p-2 text-sm text-gray-600 drop-shadow-sm"
               >
-                {language}
-              </div>
-            ))}
-          </div>
-
-          <h1 className="mt-4 text-xl">Frameworks</h1>
-          <hr className="mx-auto my-2 h-0.5 w-5/6 bg-gradient-to-r from-slate-50 via-slate-300 to-slate-50 md:mx-0 md:from-slate-300 md:via-slate-300" />
-          <div className="flex flex-wrap justify-center gap-2 py-1 pr-1 md:justify-start">
-            {FRAMEWORKS.map((framework) => (
-              <div
-                key={framework}
-                className="rounded bg-slate-200 p-2 text-sm text-gray-600 drop-shadow-sm"
-              >
-                {framework}
-              </div>
-            ))}
-          </div>
-
-          <h1 className="mt-4 text-xl">Databases</h1>
-          <hr className="mx-auto my-2 h-0.5 w-5/6 bg-gradient-to-r from-slate-50 via-slate-300 to-slate-50 md:mx-0 md:from-slate-300 md:via-slate-300" />
-          <div className="flex flex-wrap justify-center gap-2 py-1 pr-1 md:justify-start">
-            {DATABASES.map((database) => (
-              <div
-                key={database}
-                className="rounded bg-slate-200 p-2 text-sm text-gray-600 drop-shadow-sm"
-              >
-                {database}
-              </div>
-            ))}
-          </div>
-
-          <h1 className="mt-4 text-xl">Tools</h1>
-          <hr className="mx-auto my-2 h-0.5 w-5/6 bg-gradient-to-r from-slate-50 via-slate-300 to-slate-50 md:mx-0 md:from-slate-300 md:via-slate-300" />
-          <div className="flex flex-wrap justify-center gap-2 py-1 pr-1 md:justify-start">
-            {TOOLS.map((tool) => (
-              <div
-                key={tool}
-                className="rounded bg-slate-200 p-2 text-sm text-gray-600 drop-shadow-sm"
-              >
-                {tool}
-              </div>
-            ))}
-          </div>
-
-          <h1 className="mt-4 text-xl">Other</h1>
-          <hr className="mx-auto my-2 h-0.5 w-5/6 bg-gradient-to-r from-slate-50 via-slate-300 to-slate-50 md:mx-0 md:from-slate-300 md:via-slate-300" />
-          <div className="flex flex-wrap justify-center gap-2 py-1 pr-1 md:justify-start">
-            {OTHERS.map((other) => (
-              <div
-                key={other}
-                className="rounded bg-slate-200 p-2 text-sm text-gray-600 drop-shadow-sm"
-              >
-                {other}
+                {skill}
               </div>
             ))}
           </div>
