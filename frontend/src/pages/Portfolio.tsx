7792ee8f3598e2335c3cd83d4c3bbec27fc1b53a
Tuomas Kiviniemi <91017924+tkiviniemi@users.noreply.github.com>
2023-05-31 12:28:05 +0300

    Updated project list

--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -7,41 +7,66 @@ import weather_app from '../assets/weather_app.png';
 const PROJECTS = [
   {
     id: '1',
-    title: 'Marketplace App',
+    title: 'HSL Citybikes',
     description:
-      'Current WIP, a marketplace app for buying and selling second hand items.',
-    tags: ['React', 'TypeScript', 'Node.js', 'Express', 'Prisma', 'PostgreSQL'],
+      'App for exploring data from journeys made with city bikes in the Helsinki Capital area.',
+    tags: [
+      'React',
+      'TypeScript',
+      'Node.js',
+      'Express',
+      'Prisma',
+      'PostgreSQL',
+      'TailwindCSS',
+    ],
     image: ' ',
-    github: 'https://github.com/Mirxii/wearywears',
+    github: 'https://github.com/tkiviniemi/HSL_Citybikes',
   },
   {
     id: '2',
+    title: 'Wearywears',
+    description:
+      'Current WIP, a marketplace app for buying and selling second hand clothes.',
+    tags: [
+      'React',
+      'TypeScript',
+      'Node.js',
+      'Express',
+      'Prisma',
+      'PostgreSQL',
+      'MaterialUI',
+    ],
+    image: ' ',
+    github: 'https://github.com/tkiviniemi/wearywears',
+  },
+  {
+    id: '3',
     title: 'Todo App',
     description:
       'A simple todo app, implementing basic CRUD functionality for todos and users, also includes JWT-based authentication.',
     tags: ['React', 'JavaScript', 'Node.js', 'Express', 'MySQL'],
     image: todos_app,
-    github: 'https://github.com/Mirxii/todo-app',
+    github: 'https://github.com/tkiviniemi/todo-app',
     link: 'https://todo-app-mirxii.onrender.com/',
   },
   {
-    id: '3',
+    id: '4',
     title: 'My Portfolio',
     description:
       'The page you are currently on. Static website for my portfolio and resume. Built with React and TailwindCSS. Deployed to Vercel with a custom domain.',
     tags: ['React', 'TailwindCSS'],
     image: portfolio,
-    github: 'https://github.com/Mirxii/tuomaskiviniemi',
+    github: 'https://github.com/tkiviniemi/tuomaskiviniemi',
     link: 'https://tuomaskiviniemi.com/',
   },
   {
-    id: '4',
+    id: '5',
     title: 'Weather App',
     description:
       'A very simple weather app for mobile, able to lookup current weather or a 4-day forecast based on city input or location services, fetching data from OpenWeatherMap API.',
     tags: ['React Native', 'OpenWeatherMap API'],
     image: weather_app,
-    github: 'https://github.com/Mirxii/NativeWeather-App',
+    github: 'https://github.com/tkiviniemi/NativeWeather-App',
   },
 ];
 
